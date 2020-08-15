bfd9cf8e903b43ba306019e987ddfa4d5a46edf8
Hristo Manov <45336093+hmanov@users.noreply.github.com>
2020-08-15 23:42:42 +0300

    profile

--- client/src/Styled/Profile.js
+++ client/src/Styled/Profile.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
-
+import { Link } from 'react-router-dom';
 export const UserCardContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   max-width: 400px;
   width: 100%;
   max-height: 650px;
@@ -18,4 +21,32 @@ export const UserImage = styled.img`
   border-radius: 50%;
   border: 1px solid ${(props) => props.theme.primary};
 `;
-export const InfoContainer = styled.div``;
+export const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const InfoItem = styled.div`
+  color: ${(props) => props.theme.primary};
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const AdminPanelLink = styled(Link)`
+  margin-top: 100px;
+  width: 160px;
+  height: 80px;
+  text-align: center;
+  padding: 27px 10px;
+  text-decoration: none;
+  color: ${(props) => props.theme.primary};
+  font-weight: bolder;
+  border: 2px solid ${(props) => props.theme.primary};
+  border-radius: 5px;
+`;
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,19 +1,42 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthProvider';
 import { Link } from 'react-router-dom';
-import { UserCardContainer, UserImage } from '../../Styled/Profile';
+import { UserCardContainer, UserImage, InfoItem, InfoContainer, AdminPanelLink } from '../../Styled/Profile';
+import moment from 'moment';
 const Profile = () => {
   const {
     authState: {
-      user: { isAdmin },
+      user: { isAdmin, firstName, lastName, email, date },
     },
   } = useContext(AuthContext);
   return (
-    <div style={{ width: '1200px' }}>
+    <div
+      style={{
+        width: '1200px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <UserCardContainer>
         <UserImage src={require('../../assets/img/avatar.png')} />
+        <InfoContainer>
+          <InfoItem>
+            <strong>FirstName </strong>: {firstName}
+          </InfoItem>
+          <InfoItem>
+            <strong>LastName </strong>: {lastName}
+          </InfoItem>
+          <InfoItem>
+            <strong>Email </strong>: {email}
+          </InfoItem>
+          <InfoItem>
+            <strong>Registered on </strong>: {moment(date).format('DD-MM-YYYY HH:MM')}
+          </InfoItem>
+        </InfoContainer>
       </UserCardContainer>
-      {isAdmin && <Link to='/admin'>ADMIN PANEL</Link>}
+      {isAdmin && <AdminPanelLink to='/admin'>ADMIN PANEL</AdminPanelLink>}
     </div>
   );
 };
